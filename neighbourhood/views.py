857014ff6afe114591bd6634a45f69ed8dd4392d
Owilly <pasaclouma54@gmail.com>
2022-04-19 11:15:14 +0300

    add crispy forms

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -27,7 +27,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(CreateView):
     model = Post
-    fields = ['title', 'content', 'neighbourhood_location','neighbourhood_name', 'image']
+    fields = ['title', 'content', 'neighbourhood_location','neighbourhood_name']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
