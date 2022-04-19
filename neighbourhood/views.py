9740ef91baaa35416b0743be05f2102af8fc550f
Owilly <pasaclouma54@gmail.com>
2022-04-19 08:41:47 +0300

    deployment

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -26,6 +26,7 @@ class PostDetailView(DetailView):
     model = Post
 
 class PostCreateView(CreateView):
+    
     model = Post
     fields = ['title', 'content', 'neighbourhood_location','neighbourhood_name', 'image']
 
