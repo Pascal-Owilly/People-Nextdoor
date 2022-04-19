234809ce61c72d04fdde8c7c0a5fabd5b32c5237
Owilly <pasaclouma54@gmail.com>
2022-04-19 12:03:54 +0300

    add testcases

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,3 +1,53 @@
+
 from django.test import TestCase
+from .models import Profile, Post
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.pascal= Profile(neighbourhood_name = 'paclas', username ='pascalouma54@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pascal,Post))
+
+        # Testing Save Method
+    def test_save_method(self):
+        self.pascal.save_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+class ProfileTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.pascal= Profile(neighbourhood_name = 'paclas', username ='pascalouma54@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pascal,Post))
+
+        # Testing Save Method
+    def test_save_method(self):
+        self.pascal.save_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+
+class BusinessTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.pascal= Profile(neighbourhood_name = 'paclas', username ='pascalouma54@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pascal,Post))
+
+        # Testing Save Method
+    def test_save_method(self):
+        self.pascal.save_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
